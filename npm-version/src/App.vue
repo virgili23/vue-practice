16ee8a5650856d6fb30d081a7f74aca2464de312
Virgilio <vbaez@bluecorona.com>
2024-12-25 02:25:11 -0500

    added vue files

---
+++ npm-version/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+  <!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
+</template>
